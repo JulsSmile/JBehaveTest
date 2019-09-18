d05d83f9f2b230688144bf1b16aa8dea50cd5f61
Yuliia Horenko <yuliia_horenko@epam.com>
2019-09-18 14:58:53 +0300

    Test is working!

--- src/test/java/BDDTest/AcceptanceTestSuite.java
+++ src/test/java/BDDTest/AcceptanceTestSuite.java
@@ -1,7 +1,19 @@
 package BDDTest;
 
 import net.serenitybdd.jbehave.SerenityStories;
+import net.thucydides.core.webdriver.Configuration;
 
-public class AcceptanceTestSuite extends SerenityStories {
+import static net.thucydides.core.ThucydidesSystemProperty.WEBDRIVER_DRIVER;
+import static net.thucydides.core.ThucydidesSystemProperty.WEBDRIVER_PROVIDED_TYPE;
+
+public class AcceptanceTestSuite extends SerenityStories{
+    private Configuration configuration;
+
+    public AcceptanceTestSuite() {
+        configuration = super.getSystemConfiguration();
+        configuration.setIfUndefined(WEBDRIVER_DRIVER.getPropertyName(), "provided");
+        configuration.setIfUndefined(WEBDRIVER_PROVIDED_TYPE.getPropertyName(), "mydriver");
+        configuration.setIfUndefined("webdriver.provided.mydriver", "BDDTest.drivers.ChromeWebDriver");
+    }
 
 }
